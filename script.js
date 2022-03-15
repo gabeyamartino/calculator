06b76496c14acca652a73bf4a15c712d38d501cc
Gabe Yamartino <gabeyamartino@gmail.com>
2022-03-15 17:35:58 -0400

    Got the equals sign to run function operate, need to get it to return result in the display

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function divide(a, b) {
 
 function operate(a, operator, b) {
     if (operator == '+') {
-        add(a, b);
+        add(Number(a), Number(b));
     } else if (operator == '-') {
         subtract(a, b);
     } else if (operator == '*') {
@@ -32,16 +32,40 @@ function operate(a, operator, b) {
 
 operate(7, '/', 2) 
 
-const numButtons = document.querySelectorAll('button');
+const numButtons = document.querySelectorAll('button.number');
 const screen = document.getElementById('calculator-screen');
-screen.textContent = 'button.value';
-const container = document.querySelector(".container");
+const operators = document.querySelectorAll('button.operator');
+const equalSign = document.querySelectorAll('button.equal-sign');
+let  displayValue = [];
+const firstOperand = [];
+const operatorStorage = [0];
+const secondOperand = [];
+const equal = [];
 
-console.log(numButtons)
-console.log(screen)
 numButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        screen.value = button.value;
-        console.log(button.value)
+        displayValue.push(button.value);
+        screen.value = parseInt(displayValue.join(''));
     });
 });
+
+operators.forEach((button) => {
+    button.addEventListener('click', () => {
+        operatorStorage[0] = button.value;
+        firstOperand.push(parseInt(displayValue.join('')));
+        displayValue = [];
+        // the code below works, figure out second operand 
+    });
+});
+
+equalSign.forEach((button) => {
+    button.addEventListener ('click', () => {
+    secondOperand.push(parseInt(displayValue.join('')));
+    displayValue.push(operate(firstOperand, operatorStorage, secondOperand));
+    });
+});
+console.log(firstOperand);
+console.log(operatorStorage);
+console.log(secondOperand);
+
+//right now, + is just conatenating the strings.
