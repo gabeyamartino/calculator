f714b23b74cd6e290ceadf3ea05065ff41e7c0de
Gabe Yamartino <gabeyamartino@gmail.com>
2022-03-17 19:40:30 -0400

    Got the calculator to store the operate() result as first operand, allow it to be operated on again.

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
+const numButtons = document.querySelectorAll('button.number');
+const screen = document.getElementById('calculator-screen');
+const operators = document.querySelectorAll('button.operator');
+const equalSign = document.querySelectorAll('button.equal-sign');
+let displayValue = [];
+const firstOperand = [];
+const operatorStorage = [0];
+const secondOperand = [];
+
+
 function add(a, b) {
-    console.log(a + b);
+    screen.value = (a + b);
     return a + b;
 };
 
 function subtract(a, b) {
-    console.log(a - b);
+    screen.value = (a - b);
     return a - b;
 };
 
 function multiply(a, b) {
-    console.log(a * b);
+    screen.value = (a * b);
     return a * b;
 };
 
 function divide(a, b) {
-    console.log(a / b);
+    screen.value = (a / b);
     return a / b;
 };
 
 function operate(a, operator, b) {
     if (operator == '+') {
-        add(Number(a), Number(b));
+        return add(Number(a), Number(b));
     } else if (operator == '-') {
-        subtract(a, b);
+        return subtract(a, b);
     } else if (operator == '*') {
-        multiply(a, b);
+        return multiply(a, b);
     } else if (operator == '/') {
-        divide(a, b);
+        return divide(a, b);
     }
 };
-
-operate(7, '/', 2) 
-
-const numButtons = document.querySelectorAll('button.number');
-const screen = document.getElementById('calculator-screen');
-const operators = document.querySelectorAll('button.operator');
-const equalSign = document.querySelectorAll('button.equal-sign');
-let  displayValue = [];
-const firstOperand = [];
-const operatorStorage = [0];
-const secondOperand = [];
-const equal = [];
 
 numButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -52,20 +50,25 @@ numButtons.forEach((button) => {
 operators.forEach((button) => {
     button.addEventListener('click', () => {
         operatorStorage[0] = button.value;
-        firstOperand.push(parseInt(displayValue.join('')));
+        firstOperand[0] = screen.value;
         displayValue = [];
-        // the code below works, figure out second operand 
     });
 });
 
 equalSign.forEach((button) => {
     button.addEventListener ('click', () => {
-    secondOperand.push(parseInt(displayValue.join('')));
-    displayValue.push(operate(firstOperand, operatorStorage, secondOperand));
+    secondOperand[0] = screen.value;
+    firstOperand[0] = (operate(firstOperand, operatorStorage, secondOperand));
     });
 });
-console.log(firstOperand);
-console.log(operatorStorage);
-console.log(secondOperand);
 
-//right now, + is just conatenating the strings.
+// now, figure out how to perform multiple operations without pressing =
+// each time an operator is clicked, the result should display, and become the
+// firstOperand
+
+
+
+
+console.log(firstOperand)
+console.log(operatorStorage)
+console.log(secondOperand)
